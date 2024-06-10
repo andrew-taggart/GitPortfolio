3bd52c01c88385f299d8c88f21e15bdb5e672316
andrew-taggart <andrew.taggart53@gmail.com>
2024-06-10 16:02:57 -0400

    updated github links

--- myPortfolio/src/components/pages/Projects.jsx
+++ myPortfolio/src/components/pages/Projects.jsx
@@ -1,10 +1,10 @@
 
 const projects = [
-    { id: 1, title: 'Project 1', description: 'Replace with Description and add Link', url: 'https://link-to-project1.com', image: 'temporary.jpg' },
-    { id: 2, title: 'Project 2', description: 'Replace with Description and add Link', url: 'https://link-to-project2.com', image: 'temporary.jpg' },
-    { id: 3, title: 'Project 3', description: 'Replace with Description and add Link', url: 'https://link-to-project3.com', image: 'temporary.jpg' },
-    { id: 4, title: 'Project 4', description: 'Replace with Description and add Link', url: 'https://link-to-project4.com', image: 'temporary.jpg' },
-    { id: 5, title: 'Project 5', description: 'Replace with Description and add Link', url: 'https://link-to-project5.com', image: 'temporary.jpg' },
+    { id: 1, title: 'Ticketly', description: 'Ticket Venue Application. Group Project. CHANGE PHOTO.', url: 'https://github.com/Lane17027/Ticketly', image: 'Ticketly.png' },
+    { id: 2, title: 'DADA', description: 'Task Manager. Group Project.', url: 'https://github.com/andrew-taggart/DADA', image: 'DADA.png' },
+    { id: 3, title: 'Workout Planner', description: 'Workout Planner. Solo Project. MERN', url: 'https://github.com/andrew-taggart/Workout-Planner', image: 'WorkoutOrganizer.png' },
+    { id: 4, title: 'Pro/Fit Book', description: 'Appointment Scheduling Application', url: 'https://github.com/andrew-taggart/fit_book', image: 'temporary.jpg' },
+    { id: 5, title: 'FoodReactions', description: 'Replace with Description', url: 'codydecoder/FoodReactions', image: 'FoodReactionsImg.png' },
     { id: 6, title: 'Project 6', description: 'Replace with Description and add Link', url: 'https://link-to-project6.com', image: 'temporary.jpg' },
 ]
 
@@ -14,26 +14,29 @@ const Projects = () => {
             <h2>Projects</h2>
             <div className="gallery">
                 {projects.map((project) => (
-                    <div key={project.id} className="project-card">
-                        
-                        <img src={`../images/${project.image}`} alt={`Add Image ${project.id}`} />
-                        <div className="project-info">
-                            <h3>{project.title}</h3>
-                            <p>{project.description}</p>
+                    <a key={project.id} href={project.url}>
+                        <div key={project.id} className="project-card">
+
+                            <img src={`../images/${project.image}`} alt={`Add Image ${project.id}`} />
+                            <div className="project-info">
+                                <h3>{project.title}</h3>
+                                <p>{project.description}</p>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 ))}
             </div>
 
             <div className="temporary">
                 <ul>
                     <h4>To Do for this Page</h4>
-                    <li>Links (launch site) and git?</li>
+                    <li>Launch Demo sites??</li>
                     <li>Description of Projects
                         <ul>
                             <li>50-100 words</li>
                             <li>service/purpose</li>
                             <li>group or solo</li>
+                            <li>GitHub link if launching all projects</li>
                         </ul>
                     </li>
                 </ul>
